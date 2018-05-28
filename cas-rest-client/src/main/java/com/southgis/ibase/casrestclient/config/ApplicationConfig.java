a34c0af9e0e92686f9c86b988e574edb6fac8347
dongji <550111817@qq.com>
2018-05-28 11:58:48 +0800

    cas management 服务管理

--- cas-rest-client/src/main/java/com/southgis/ibase/casrestclient/config/ApplicationConfig.java
+++ cas-rest-client/src/main/java/com/southgis/ibase/casrestclient/config/ApplicationConfig.java
@@ -18,6 +18,7 @@ public class ApplicationConfig {
         SysUser locked = new SysUser().setUsername("rest-locked").setPassword("202cb962ac59075b964b07152d234b70").setLocked(true);
         SysUser disable = new SysUser().setUsername("rest-disable").setPassword("202cb962ac59075b964b07152d234b70").setDisable(true);
         SysUser expired = new SysUser().setUsername("rest-expired").setPassword("202cb962ac59075b964b07152d234b70").setExpired(true);
-        return new UserRepertory(admin, test, locked, disable, expired);
+        SysUser admin2 = new SysUser().setUsername("admin").setPassword("202cb962ac59075b964b07152d234b70").addAttribute("key", "keyVal");
+        return new UserRepertory(admin, test, locked, disable, expired, admin2);
     }
 }
